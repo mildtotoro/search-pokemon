8a98aacc9ea2f05bf44fc7f1d237c98fd2fe3520
mildtotoro <pimrampha.m@gmail.com>
2021-08-01 11:40:32 +0700

    create compoment pekemon

---
+++ src/components/Pokemon.tsx
@@ -0,0 +1,165 @@
+import { PokemonInterface } from "../types";
+import { Link } from "react-router-dom";
+
+const Pokemon = ({ pokemon }: { pokemon: PokemonInterface }) => {
+  return (
+    <div className="relative m-5 sm:w-3/5 mx-auto">
+      <div className="sm:absolute bg-white p-4 h-32 w-full sm:w-32 top-5 -left-5 border border-solid rounded-sm drop-shadow-md border-gray-300">
+        <div
+          className="w-full h-full bg-contain bg-no-repeat bg-center"
+          style={{ backgroundImage: `url(${pokemon.image})` }}
+        ></div>
+      </div>
+      <div className=" p-5 sm:pl-32 text-left border-solid border rounded-sm border-gray-300">
+        <h2 className="text-yellow-600 uppercase font-bold inline">
+          {pokemon.name}
+        </h2>
+
+        <h4
+          data-testid="pokemon-types"
+          className=" text-gray-500 font-light text-xs inline pl-3"
+        >
+          (
+          {pokemon.types &&
+            pokemon.types.map((item, i) => {
+              // const comma = i < pokemon.types.length - 1 ? ", " : "";
+              // const text = item + comma;
+              return <span key={item}>{item}</span>;
+            })}
+          )
+        </h4>
+        <div className="mt-5 grid grid-cols-5 gap-x-1 gap-y-2 text-xs text-gray-600">
+          <div className="font-extralight">Max HP</div>
+          <div className="col-span-4">
+            <h5 className="font-light">{pokemon.maxHP}</h5>
+          </div>
+          {/*  */}
+          <div className="font-extralight">Max CP</div>
+          <div className="col-span-4">
+            <h5 className="font-light">{pokemon.maxCP}</h5>
+          </div>
+          {/*  */}
+          <div className="font-extralight">Flee Rate</div>
+          <div className="col-span-4">
+            <h5 className="font-light">{pokemon.fleeRate}</h5>
+          </div>
+
+          {/*  */}
+          <div className="font-extralight">Weight</div>
+          <div className="col-span-4">
+            <h5 className="font-light">
+              {pokemon.weight?.minimum} - {pokemon.weight?.maximum}
+            </h5>
+          </div>
+          {/*  */}
+          <div className="font-extralight">Height</div>
+          <div className="col-span-4">
+            <h5 className="font-light">
+              {pokemon.height?.minimum} - {pokemon.height?.maximum}
+            </h5>
+          </div>
+          {/*  */}
+          <div className="font-extralight">Classification</div>
+          <div className="col-span-4">
+            <h5 className="font-light">{pokemon.classification}</h5>
+          </div>
+
+          {/*  */}
+          <div className="font-extralight">Resistant</div>
+          <div className="col-span-4">
+            <h5 className="font-light">
+              {pokemon.resistant &&
+                pokemon.resistant.map((item, i) => {
+                  // const comma = i < pokemon.resistant.length - 1 ? ", " : "";
+                  // const text = item + comma;
+                  return <span key={item}>{item}</span>;
+                })}
+            </h5>
+          </div>
+          {/*  */}
+          <div className="font-extralight">Attacks</div>
+          <div className="col-span-4">
+            <div className="flex">
+              <h5 className="font-light flex-1">
+                <div className="mb-2">Fast</div>
+
+                {pokemon.attacks &&
+                  pokemon.attacks.fast.map((item) => {
+                    return (
+                      <div className="font-light" key={item.name}>
+                        <span className="font-extralight">{item.name}</span> (
+                        {item.type}) ({item.damage})
+                      </div>
+                    );
+                  })}
+              </h5>
+              <h5 className="font-light flex-1">
+                <div className="mb-2">Special </div>
+
+                {pokemon.attacks &&
+                  pokemon.attacks.special.map((item) => {
+                    return (
+                      <div className="font-light" key={item.name}>
+                        <span className="font-extralight">{item.name}</span> (
+                        {item.type}) ({item.damage})
+                      </div>
+                    );
+                  })}
+              </h5>
+            </div>
+          </div>
+          {/*  */}
+          <div className="font-extralight">Weaknesses</div>
+          <div className="col-span-4">
+            <h5 className="font-light">
+              {pokemon.weaknesses &&
+                pokemon.weaknesses.map((item, i) => {
+                  // const comma = i < pokemon.weaknesses.length - 1 ? ", " : "";
+                  // const text = item + comma;
+                  return <span key={item}>{item}</span>;
+                })}
+            </h5>
+          </div>
+
+          {/*  */}
+          <div className="font-extralight">Evolutions</div>
+          <div className="col-span-4">
+            <h5 className="font-light">
+              {pokemon.evolutions &&
+                pokemon.evolutions.map((item) => {
+                  const params = new URLSearchParams({ name: item.name });
+                  return (
+                    <Link
+                      key={item.id}
+                      className="pr-2 underline"
+                      to={{
+                        search: params.toString(),
+                      }}
+                    >
+                      {item.name}
+                    </Link>
+                  );
+                })}
+            </h5>
+          </div>
+          {/*  */}
+          <div className="font-extralight">Evolution Requirements</div>
+          <div className="col-span-4">
+            {pokemon.evolutionRequirements && (
+              <>
+                <h5 className="font-light">
+                  {pokemon.evolutionRequirements.name}{" "}
+                  {pokemon.evolutionRequirements.amount} ea
+                </h5>
+              </>
+            )}
+          </div>
+
+          {/*  */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Pokemon;
--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { gql, useLazyQuery } from "@apollo/client";
 import { PokemonDataInterface } from "../types";
+import Pokemon from "../components/Pokemon";
+import { useLocation, useHistory } from "react-router-dom";
 
 const GET_POKEMON = gql`
   query pokemon($id: String, $name: String) {
@@ -24,6 +26,18 @@ const GET_POKEMON = gql`
       maxCP
       maxHP
       image
+      attacks {
+        fast {
+          name
+          type
+          damage
+        }
+        special {
+          name
+          type
+          damage
+        }
+      }
       evolutions {
         id
         name
@@ -36,172 +50,61 @@ const GET_POKEMON = gql`
   }
 `;
 
-function Search() {
-  const [name, setName] = useState("Squirtle");
+function useQueryParam() {
+  return new URLSearchParams(useLocation().search);
+}
 
-  const [searchPokemon, { loading, data, error }] =
+function Search() {
+  const query = useQueryParam();
+  const history = useHistory();
+  const nameParam = query.get("name");
+  const [name, setName] = useState(nameParam || "");
+
+  const [searchPokemon, { loading, data, error, called }] =
     useLazyQuery<PokemonDataInterface>(GET_POKEMON);
+  useEffect(() => {
+    console.log("use effect", nameParam);
+    if (nameParam !== "" && nameParam !== null) {
+      setName(nameParam);
+      searchPokemon({
+        variables: { name: nameParam },
+      });
+    }
+  }, [nameParam, searchPokemon]);
 
   const pokemon = data?.pokemon;
-  console.log({ data });
   if (loading) return <div>Loading</div>;
   if (error) return <div>{error.message}</div>;
   return (
-    <div>
+    <div className="pt-5">
       <form
         method="GET"
         onSubmit={(e) => {
           e.preventDefault();
-          searchPokemon({
-            variables: { name: name },
-          });
+          if (nameParam !== name) {
+            const params = new URLSearchParams({ name: name });
+            history.replace({ search: params.toString() });
+          }
         }}
       >
         <input
-          className="border"
+          className="border text-gray-500  text-sm font-light p-2"
           type="text"
           name="name"
           onChange={(e) => setName(e.target.value)}
           value={name}
         />
-        <button type="submit">ok</button>
+        <button
+          type="submit"
+          className="border text-gray-500 text-sm font-light p-2"
+        >
+          Search
+        </button>
       </form>
       {pokemon ? (
-        <div className="relative m-5 sm:w-3/5 mx-auto">
-          <div className="sm:absolute bg-white p-4 h-32 w-full sm:w-32 top-5 -left-5 border border-solid rounded-sm drop-shadow-md border-gray-300">
-            <div
-              className="w-full h-full bg-contain bg-no-repeat bg-center"
-              style={{ backgroundImage: `url(${pokemon.image})` }}
-            ></div>
-          </div>
-          <div className=" p-5 sm:pl-32 text-left border-solid border rounded-sm border-gray-300">
-            <h2 className="text-yellow-600 uppercase">{pokemon.name}</h2>
-            <div className="grid grid-cols-5 gap-x-1 text-xs text-gray-600">
-              <div className="">Weight</div>
-              <div className="col-span-4">
-                <h5 className=" ">
-                  {pokemon.weight.minimum}-{pokemon.weight.maximum}
-                </h5>
-              </div>
-              {/*  */}
-              <div className="">Height</div>
-              <div className="col-span-4">
-                <h5 className=" ">
-                  {pokemon.height.minimum}-{pokemon.height.maximum}
-                </h5>
-              </div>
-              {/*  */}
-              <div className="">Classification</div>
-              <div className="col-span-4">
-                <h5 className=" ">{pokemon.classification}</h5>
-              </div>
-              {/*  */}
-              <div className="">Type</div>
-              <div className="col-span-4">
-                <h5 className=" ">
-                  {pokemon.types &&
-                    pokemon.types.map((item) => {
-                      return item + " ";
-                    })}
-                </h5>
-              </div>
-              {/*  */}
-              <div className="">Resistant</div>
-              <div className="col-span-4">
-                <h5 className=" ">
-                  {pokemon.resistant &&
-                    pokemon.resistant.map((item) => {
-                      return item + " ";
-                    })}
-                </h5>
-              </div>
-              {/*  */}
-              <div className="">Attacks</div>
-              <div className="col-span-4">
-                <h5 className=" ">
-                  <div>fast</div>
-
-                  {pokemon.attacks &&
-                    pokemon.attacks.fast.map((item) => {
-                      return (
-                        <div>
-                          name: {item.name}
-                          type: {item.type}
-                          damage: {item.damage}
-                        </div>
-                      );
-                    })}
-                </h5>
-                <h5 className=" ">
-                  <div>Special</div>
-
-                  {pokemon.attacks &&
-                    pokemon.attacks.special.map((item) => {
-                      return (
-                        <div>
-                          name: {item.name}
-                          type: {item.type}
-                          damage: {item.damage}
-                        </div>
-                      );
-                    })}
-                </h5>
-              </div>
-              {/*  */}
-              <div className="">Weaknesses</div>
-              <div className="col-span-4">
-                <h5 className=" ">
-                  {pokemon.weaknesses &&
-                    pokemon.weaknesses.map((item) => {
-                      return item + " ";
-                    })}
-                </h5>
-              </div>
-
-              {/*  */}
-              <div className="">FleeRate</div>
-              <div className="col-span-4">
-                <h5 className=" ">{pokemon.fleeRate}</h5>
-              </div>
-              {/*  */}
-              <div className="">Max CP</div>
-              <div className="col-span-4">
-                <h5 className=" ">{pokemon.maxCP}</h5>
-              </div>
-              {/*  */}
-              <div className="">evolutions</div>
-              <div className="col-span-4">
-                <h5 className=" ">
-                  {pokemon.evolutions &&
-                    pokemon.evolutions.map((item) => {
-                      return item.name;
-                    })}
-                </h5>
-              </div>
-              {/*  */}
-              <div className="">evolutionRequirements</div>
-              <div className="col-span-4">
-                {pokemon.evolutionRequirements && (
-                  <>
-                    <h5 className=" ">
-                      Amount{pokemon.evolutionRequirements.amount}
-                    </h5>
-                    <h5 className=" ">
-                      Name{pokemon.evolutionRequirements.name}
-                    </h5>
-                  </>
-                )}
-              </div>
-              <div className="">Max HP</div>
-              <div className="col-span-4">
-                <h5 className=" ">{pokemon.maxHP}</h5>
-              </div>
-              {/*  */}
-            </div>
-          </div>
-        </div>
+        <Pokemon pokemon={pokemon}></Pokemon>
       ) : (
-        <div>not found</div>
+        called && <div>not found</div>
       )}
     </div>
   );
